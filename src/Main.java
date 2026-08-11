54fa53564988f75efc3070f12b82be932960c3c1
Jocs <106163330+victorravazi@users.noreply.github.com>
2026-08-10 22:20:40 -0300

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        float subtotal = 80;
-        float distancia = 5;
-
-        RegraNegocio calcular = new RegraNegocio();
-        System.out.println(calcular.calcularFreteFormatado(subtotal, distancia));
-    }
-}
